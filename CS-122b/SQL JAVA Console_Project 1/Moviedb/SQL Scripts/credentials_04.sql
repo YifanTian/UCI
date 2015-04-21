0d0394550098e8b99e08b3b91baba4fb06d1b1e7
vrshah90 <vrshah90@users.noreply.github.com>
2015-04-20 17:42:44 -0700

    Update credentials_04.sql
    Added other credentials

--- CS-122b/SQL JAVA Console_Project 1/Moviedb/SQL Scripts/credentials_04.sql
+++ CS-122b/SQL JAVA Console_Project 1/Moviedb/SQL Scripts/credentials_04.sql
@@ -5,3 +5,11 @@ GRANT ALL PRIVILEGES ON *.* TO 'dbuser'@'localhost' WITH GRANT OPTION;
 CREATE USER 'dbuser'@'%' IDENTIFIED BY 'dbpassword';
 
 GRANT ALL PRIVILEGES ON *.* TO 'dbuser'@'%' WITH GRANT OPTION;
+
+CREATE USER 'testuser'@'localhost' IDENTIFIED BY 'testpass';
+
+GRANT ALL PRIVILEGES ON *.* TO 'testuser'@'localhost' WITH GRANT OPTION;
+
+CREATE USER 'testuser'@'%' IDENTIFIED BY 'testpass';
+
+GRANT ALL PRIVILEGES ON *.* TO 'testuser'@'%' WITH GRANT OPTION;
